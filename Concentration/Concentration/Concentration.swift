458e1123d05020918541955b55362f9ca633344c
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-04-06 22:41:52 +0300

    finish second lesson

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -11,9 +11,28 @@ import Foundation
 class Concentration {
     
     var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
     
     func chooseCard(at index: Int) {
-        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+                
+                
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Foundation
+
+
 
 class ViewController: UIViewController {
     
@@ -15,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet var flipCountLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
     
-    var emojiChoices = ["👻", "🎃", "👻", "🎃"]
+    
     
     var flipCount = 0 {
         didSet {
@@ -23,32 +26,62 @@ class ViewController: UIViewController {
         }
     }
     
-     @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
-        let cardNumber = cardButtons.firstIndex(of: sender)!
-        print("card number \(cardNumber)")
-        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+        //print("card number \(cardNumber)")
+        //        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        
+    }
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.441413555, blue: 0.261328571, alpha: 0) : #colorLiteral(red: 1, green: 0.441413555, blue: 0.261328571, alpha: 1)
+            }
+        }
+        
         
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle("", for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    var emoji = [Int: String]()
+    var emojiChoices = ["🦇", "😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
+    
+    func emoji(for card: Card) -> String {
+        //let chosenEmoji = emoji[card.identifier]
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
         }
+        
+        return emoji[card.identifier] ?? "?"
+        
     }
     
     
     
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
 
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
